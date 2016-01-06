511410b2c22a6be3d9aba6bf4b96869fa37f8a16
subinsv <subinsv911@gmail.com>
2016-01-06 21:50:50 +0530

    Path fix

--- routes/test/test.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/',function(req,res,nect){
-	res.render('guest/index');
-});
-module.exports = router;
