942a135c1ecdef0a80e8dee2287e4401fbc44b17
subinsv <subinsv911@gmail.com>
2016-01-06 14:44:59 +0530

    route guest/index edited

--- routes/Guest/index.js
+++ routes/Guest/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('Guest/index');
+  res.render('Guest/index.ejs');
 });
 
 module.exports = router;
